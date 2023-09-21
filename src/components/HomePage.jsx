cdff12e3bf404b6551b06ed48abfabba1879d726
DivyaRaj258 <divya.r258@gmail.com>
2023-09-21 19:37:41 +0200

    edited the mainpage

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+// const getProject = () => {          
+//     // axios
+//     //   .get(`${API_URL}/api/projects/${projectId}`,
+//     //   { headers: { Authorization: `Bearer ${storedToken}` } }
+//     projectsService.getProject(projectId)  
+//       .then((response) => {
+//         const oneProject = response.data;
+//         setProject(oneProject);
+//       })
+//       .catch((error) => console.log(error));
+//   };
