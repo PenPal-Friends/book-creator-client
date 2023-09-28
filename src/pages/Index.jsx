7dab277970f1ccb17eadf5460b2d1cf7a6a7f18e
DivyaRaj258 <divya.r258@gmail.com>
2023-09-28 23:55:55 +0200

    some css

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,16 +1,38 @@
-
 // If NOT logged in, display:
 
 function Index() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to PenPal & Friends</p>
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <div className="bg-gray-300 text-white w-1/1.5 p-2">
+        <h1 className="text-2xl font-bold">PenPal & Friends</h1>
+        {/* Add links, navigation, or other sidebar content here */}
+      </div>
+      
+
+       {/* Main Content Area */}
+       <div className="flex-grow p-4">
+        <div className="flex">
+          {/* Text Box (Left) */}
+          <div className="w-1/2 p-4">
+            <h2 className="text-xl font-semibold">Write Your nonfiction Book, lighting fast</h2>
+            <p>Add your text content here.</p>
+          </div>
+          {/* Image Box (Right) */}
+          <div className="w-1/2 p-4">
+            <h2 className="text-xl font-semibold">Image Box</h2>
+            <img
+              src="your-image-url.jpg"
+              alt="Image"
+              className="w-full h-auto rounded-lg"
+            />
+          </div>
+        </div>
+      </div>
+    <div className="bg-gray-300 text-white p-4 text">
+      &copy; {new Date().getFullYear()} PenPal & Friends
+    </div>
     </div>
   );
 }
-
 export default Index;
-
-// If logged in, display:
-// Book page
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -34,34 +34,116 @@ function SignUp(props) {
       });
   };
 
+
   return (
-    <div className="SignUp">
-      <h1>Sign Up</h1>
-
-      <form onSubmit={handleSignupSubmit}>
-        <label>Email:</label>
-        <input type="email" name="email" value={email} onChange={handleEmail} />
-
-        <label>Password:</label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={handlePassword}
+    <div className="flex min-h-full flex-col justify-left px-6 py-12 lg:px-8">
+    <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <h2
+          className="mx-auto h-10 w-auto"
+          
+          alt="PenPal & Friends"
         />
+        <h2 className="mt-25 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign Up to create a new account.
+        </h2>
+      </div>
 
-        <label>Name:</label>
-        <input type="text" name="name" value={name} onChange={handleName} />
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form className="space-y-6" onSubmit={handleSignupSubmit}>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <div className="mt-1">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  value={email}
+                  onChange={handleEmail}
+                />
+              </div>
+            </div>
 
-        <button type="submit">Sign Up</button>
-      </form>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  value={password}
+                  onChange={handlePassword}
+                />
+              </div>
+            </div>
 
-      {errorMessage && <p className="error-message">{errorMessage}</p>}
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Name
+              </label>
+              <div className="mt-1">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  autoComplete="name"
+                  required
+                  className="appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  value={name}
+                  onChange={handleName}
+                />
+              </div>
+            </div>
 
-      <p>Already have account?</p>
-      <Link to={"/login"}> Login</Link>
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Sign Up
+              </button>
+            </div>
+          </form>
+
+          {errorMessage && (
+            <p className="mt-2 text-sm text-blue-600">{errorMessage}</p>
+          )}
+
+          <div className="mt-6">
+            <p className="text-sm text-center text-gray-500">
+              Already have an account?{" "}
+              <Link
+                to={"/login"}
+                className="font-medium text-green-600 hover:text-blue-500"
+              >
+                Login
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default SignUp;
+
